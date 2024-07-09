844720ca315b38eb2c665f4ebc1a024f93b20f4a
Jay Kid <cyph3rfox@gmail.com>
2024-07-09 16:10:20 +0200

    feat: use a styled component for the App to showcase usage

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import './App.css';
 import TreeViewer from './components/TreeViewer';
+import { styled } from '@mui/system';
+
+const MainContainer = styled('main')({
+  maxWidth: "1280px",
+  margin: "0 auto",
+  padding: "2rem"
+});
 
 function App() {
     return (
-        <main id="content">
+        <MainContainer id="content">
             <header>
                 <h1>Natzka coding challenge</h1>
             </header>
             <TreeViewer />
-        </main>
+        </MainContainer>
     );
 }
 
